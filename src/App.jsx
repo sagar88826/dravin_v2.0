6aa4c55758196521bccb2c32ccc5e1d11c2e9b0d
sagar88826 <sagarmandal88826@gmail.com>
2023-01-28 21:05:39 +0530

    a

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/login/Login";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
